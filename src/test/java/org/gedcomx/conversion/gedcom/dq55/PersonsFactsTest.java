df266a6eb78317463db7de9cf85e14afa234664e
dchamberlain <chamberlaindb@familysearch.org>
2012-05-18 14:27:03 -0600

    Initial facts unit tests.

--- src/test/java/org/gedcomx/conversion/gedcom/dq55/PersonsFactsTest.java
+++ src/test/java/org/gedcomx/conversion/gedcom/dq55/PersonsFactsTest.java
@@ -3,19 +3,15 @@ package org.gedcomx.conversion.gedcom.dq55;
 import org.folg.gedcom.model.Gedcom;
 import org.folg.gedcom.model.Person;
 import org.folg.gedcom.parser.ModelParser;
-import org.gedcomx.conclusion.NameForm;
-import org.gedcomx.conclusion.NamePart;
 import org.gedcomx.conversion.GedcomxConversionResult;
-import org.gedcomx.types.GenderType;
-import org.gedcomx.types.NamePartType;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.File;
 import java.net.URL;
 
-import static org.testng.Assert.*;
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
 
 
 public class PersonsFactsTest {
@@ -35,7 +31,7 @@ public class PersonsFactsTest {
 
   @Test
   public void testToPerson1() throws Exception {
-    // Sex M
+    // Birth and Death - Dates and places
     Person dqPerson = gedcom.getPeople().get(0);
     GedcomxConversionResult result = new GedcomxConversionResult();
     PersonMapper mapper = new PersonMapper();
@@ -45,54 +41,6 @@ public class PersonsFactsTest {
     assertEquals(result.getPersons().size(), 1);
 
     org.gedcomx.conclusion.Person person = result.getPersons().get(0);
-    assertNotNull(person.getGender());
-    assertEquals(person.getGender().getKnownType(), GenderType.Male);
-  }
-
-  @Test
-  public void testToPerson2() throws Exception {
-    // Sex F
-    Person dqPerson = gedcom.getPeople().get(1);
-    GedcomxConversionResult result = new GedcomxConversionResult();
-    PersonMapper mapper = new PersonMapper();
-
-    mapper.toPerson(dqPerson, result);
-    assertNotNull(result.getPersons());
-    assertEquals(result.getPersons().size(), 1);
-
-    org.gedcomx.conclusion.Person person = result.getPersons().get(0);
-    assertNotNull(person.getGender());
-    assertEquals(person.getGender().getKnownType(), GenderType.Female);
-  }
-
-  @Test
-  public void testToPerson3() throws Exception {
-    // Sex U
-    Person dqPerson = gedcom.getPeople().get(2);
-    GedcomxConversionResult result = new GedcomxConversionResult();
-    PersonMapper mapper = new PersonMapper();
-
-    mapper.toPerson(dqPerson, result);
-    assertNotNull(result.getPersons());
-    assertEquals(result.getPersons().size(), 1);
-
-    org.gedcomx.conclusion.Person person = result.getPersons().get(0);
-    assertNotNull(person.getGender());
-    assertEquals(person.getGender().getKnownType(), GenderType.Unknown);
-  }
-
-  @Test
-  public void testToPerson4() throws Exception {
-    // Sex INVALID
-    Person dqPerson = gedcom.getPeople().get(3);
-    GedcomxConversionResult result = new GedcomxConversionResult();
-    PersonMapper mapper = new PersonMapper();
-
-    mapper.toPerson(dqPerson, result);
-    assertNotNull(result.getPersons());
-    assertEquals(result.getPersons().size(), 1);
-
-    org.gedcomx.conclusion.Person person = result.getPersons().get(0);
-    assertNull(person.getGender());
+    assertEquals(person.getNames().get(0).getPrimaryForm().getFullText(), "Petrus Marinus Louwaert");
   }
 }
